7fd6aa1e147c8439c697f7603d7dc4a7e3f5adbb
Akihiro Kosugi <a1kosugi@jp.ibm.com>
2020-09-13 21:44:17 +0900

    adding destinations in Hakozaki office.

--- HLPDialog/conv_cabot_en.swift
+++ HLPDialog/conv_cabot_en.swift
@@ -12,6 +12,26 @@ class conv_cabot_en : conv_cabot{
     
     private static let elevator = try! NSRegularExpression(pattern:"elevator")
     private static let subway = try! NSRegularExpression(pattern:"subway|station")
+    
+    /*
+     - # IBM Research - Tokyo
+       title: IBM Research - Tokyo
+       value: EDITOR_node_1589780736215
+     - # 10F vending machine
+       title: 10F vending machine
+       value: EDITOR_node_1589781790959
+     - # 11F vending machine
+       title: 11F vending machine
+       value: EDITOR_node_1589781245452
+     - # IBM Japan entrance (1F)
+       title: IBM Japan entrance (1F)
+       value: EDITOR_node_1599730474821
+     */
+    private static let trl = try! NSRegularExpression(pattern:"research|Tokyo")
+    private static let vm10 = try! NSRegularExpression(pattern:"10")
+    private static let vm11 = try! NSRegularExpression(pattern:"11")
+    private static let entrance = try! NSRegularExpression(pattern:"entrance")
+    
     internal override func _get_response(_ orgtext:String?) -> [String:Any]{
         var speak:String = "Sorry, I couldn't catch you."
         var dest_info:[String:String]? = nil
@@ -25,6 +45,26 @@ class conv_cabot_en : conv_cabot{
                 speak = "OK, going to the subway station."
                 dest_info = [
                     "nodes": "EDITOR_node_1599633337007"
+                ]
+            }else if self._matches(text, regex: conv_cabot_en.trl){
+                speak = "OK, going to TRL."
+                dest_info = [
+                    "nodes": "EDITOR_node_1589780736215"
+                ]
+            }else if self._matches(text, regex: conv_cabot_en.vm10){
+                speak = "OK, going to vending machine of 10th floor."
+                dest_info = [
+                    "nodes": "EDITOR_node_1589781790959"
+                ]
+            }else if self._matches(text, regex: conv_cabot_en.vm11){
+                speak = "OK, going to vending machine of 11th floor."
+                dest_info = [
+                    "nodes": "EDITOR_node_1589781245452"
+                ]
+            }else if self._matches(text, regex: conv_cabot_en.entrance){
+                speak = "OK, going to the entrance."
+                dest_info = [
+                    "nodes": "EDITOR_node_1599730474821"
                 ]
             }
         }else{
