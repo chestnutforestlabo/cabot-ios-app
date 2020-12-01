1a54482f7981c13d98469da21b6f631f743545ba
Akihiro Kosugi <a1kosugi@jp.ibm.com>
2020-12-01 14:32:47 +0900

    coredo3 destinations in dialogue

---
+++ CaBot/dlgviewcontroller_coredo3.swift
@@ -0,0 +1,15 @@
+//
+//  dlgviewcontroller_coredo3.swift
+//  CaBot
+//
+//  Created by akhrksg on 2020/11/30.
+//  Copyright © 2020 CMU. All rights reserved.
+//
+
+import Foundation
+
+class dlgviewcontroller_coredo3 : dlgviewcontroller_cabot{
+    internal override func getconversation(pre: Locale) -> conv_cabot{
+        return conv_cabot_coredo3()
+    }
+}
