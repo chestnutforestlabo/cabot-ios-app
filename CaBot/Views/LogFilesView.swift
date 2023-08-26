8fb3abb2280af54d7e9abfd4c5fa47794e3e138d
Seita Kayukawa <Seita.Kayukawa@ibm.com>
2023-08-27 08:44:49 +0900

    display a report submission form via a sheet

--- CaBot/Views/LogFilesView.swift
+++ CaBot/Views/LogFilesView.swift
@@ -8,31 +8,38 @@
 
 import SwiftUI
 
+
 struct LogFilesView: View {
     @Environment(\.locale) var locale: Locale
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @EnvironmentObject var modelData: CaBotAppModel
 
     @State var langOverride:String
-
+    @State private var isShowingSheet = false
+    @State private var selectedLogFile = ""
     var body: some View {
         // Load log file list from CaBot
         let logFileList = ["cabot_2023-9-1-12-00-00","cabot_2023-9-1-13-00-00","cabot_2023-9-1-14-00-00"]
         let header = Text("SELECT_LOG")
-
         return Form{
             Section(
                 header: header){
                     ForEach(logFileList, id: \.self) { logFile in
-                        NavigationLink(
-                            destination: LogFileDetailView(langOverride: modelData.resourceLang, logFileName: logFile)
-                                .environmentObject(modelData),
-                            label:{Text(logFile)
+                        Button(action: {isShowingSheet.toggle()
+                            selectedLogFile = logFile
+                        },
+                               label: {Text(logFile)})
+                        .sheet(isPresented: $isShowingSheet) {
+                            if #available(iOS 15.0, *) {
+                                ReportSubmissionForm(logFileName: selectedLogFile)
+                            } else {
+                                // Fallback on earlier versions
                             }
-                        )
+                        }
                     }
                 }
-        }.listStyle(PlainListStyle())
+        }
+        .listStyle(PlainListStyle())
     }
 }
 
@@ -45,5 +52,48 @@ struct LogFilesView_Previews: PreviewProvider {
         return LogFilesView(langOverride: "en-US")
             .environmentObject(modelData)
             .environment(\.locale, Locale.init(identifier: "en-US"))
+    }
+}
+
+
+@available(iOS 15.0, *)
+struct ReportSubmissionForm: View {
+    let logFileName: String
+    @Environment(\.dismiss) var dismiss
+    @State var inputTitleText = ""
+    @State var inputDetailsText = ""
+    
+    var body: some View {
+        return Form{
+            Section(
+                header: Text(logFileName)){
+                    VStack{
+                        HStack {
+                            Text("REPORT_TITLE")
+                            Spacer()
+                        }
+                        TextField("ENTER_REPORT_TITLE", text: $inputTitleText)
+                    }
+                }
+            Section(){
+                VStack{
+                    HStack {
+                        Text("REPORT_DETAILS")
+                        Spacer()
+                    }
+                    if #available(iOS 16.0, *){
+                        TextField("ENTER_REPORT_DETAILS", text: $inputDetailsText, axis: .vertical)
+                            .frame(height: 100)
+                    }else{
+                        TextField("ENTER_REPORT_DETAILS", text: $inputDetailsText)
+                            .frame(height: 100)
+                    }
+                }
+            }
+            Button(
+                action: {dismiss()})
+            {Text("SAVE_YOUR_REPORT")}
+                .disabled(inputTitleText.count==0 || inputDetailsText.count==0)
+        }
     }
 }
