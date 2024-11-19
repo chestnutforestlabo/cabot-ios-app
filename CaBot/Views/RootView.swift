825c8eb65309489d560eac0d841016da4075e8ee
Seita Kayukawa <Seita.Kayukawa@ibm.com>
2024-11-20 08:08:14 +0900

    [WIP] show connection icon in the toolbar
    Signed-off-by: Seita Kayukawa <Seita.Kayukawa@ibm.com>

--- CaBot/Views/RootView.swift
+++ CaBot/Views/RootView.swift
@@ -61,6 +61,14 @@ struct RootView: View {
                 #endif
             }
             .navigationTitle(modelData.displayedScene.text(lang: modelData.resourceLang))
+            #if ATTEND
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: modelData.suitcaseConnected ? "checkmark.circle.fill" : "xmark.circle.fill")
+                        .foregroundColor(modelData.suitcaseConnected ? .blue : .red)
+                }
+            }
+            #endif
             .sheet(isPresented: $modelData.isContentPresenting, content: {
                 if let url = modelData.contentURL {
                     VStack {
