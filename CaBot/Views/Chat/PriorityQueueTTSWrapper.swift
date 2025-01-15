967a76905904db7b747e94b5a20a0eb36705fbd3
Takashi Itoh <takashi.ito@jst.go.jp>
2025-01-15 12:19:03 +0900

    Added Japanese punctuation
    Signed-off-by: Takashi Itoh <takashi.ito@jst.go.jp>

--- CaBot/Views/Chat/PriorityQueueTTSWrapper.swift
+++ CaBot/Views/Chat/PriorityQueueTTSWrapper.swift
@@ -52,7 +52,7 @@ class PriorityQueueTTSWrapper: NSObject, TTSProtocol, PriorityQueueTTSDelegate {
 
     func speak(_ text: PassthroughSubject<String, any Error>?, callback: @escaping () -> Void) {
         guard let text = text else { return callback() }
-        let entry = TokenizerEntry(separators: [".", "!", "?", "\n"], timeout_sec: 180) { _, token, reason in
+        let entry = TokenizerEntry(separators: [".", "!", "?", "\n", "。"], timeout_sec: 180) { _, token, reason in
             Debug(log:"<TTS> complete reason:\(reason) token:\(token?.text ?? "")")
             if reason == .Canceled && token != nil {
                 callback()
