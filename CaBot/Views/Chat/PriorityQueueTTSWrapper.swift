0ab23e0651d8af64e543f71b8d7957a1e0477eb7
Takashi Itoh <takashi.ito@jst.go.jp>
2025-01-18 13:09:08 +0900

    Add more punctuation chars
    Signed-off-by: Takashi Itoh <takashi.ito@jst.go.jp>

--- CaBot/Views/Chat/PriorityQueueTTSWrapper.swift
+++ CaBot/Views/Chat/PriorityQueueTTSWrapper.swift
@@ -70,7 +70,7 @@ class PriorityQueueTTSWrapper: NSObject, TTSProtocol, PriorityQueueTTSDelegate {
 
     func speak(_ text: PassthroughSubject<String, any Error>?, callback: @escaping () -> Void) {
         guard let text = text else { return callback() }
-        let entry = TokenizerEntry(separators: [".", "!", "?", "\n", "。"], timeout_sec: 180) { _, token, reason in
+        let entry = TokenizerEntry(separators: [".", "!", "?", "\n", "。", "！", "？"], timeout_sec: 180) { _, token, reason in
             Debug(log:"<TTS> complete reason:\(reason) token:\(token?.text ?? "")")
             if reason != .Canceled, let token = token, let text = token.text {
                 let voiceover = UIAccessibility.isVoiceOverRunning
